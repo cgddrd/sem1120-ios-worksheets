a9c65a8a19611109196740d7f10464567e0c482c
Connor Goddard <hello@connorlukegoddard.com>
2016-02-12 15:59:51 +0000

    Initial commit of second example project 'ColourNamer'.

---
+++ ColourNamer/ColourNamer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColourNamer
+//
+//  Created by Connor Goddard on 12/02/2016.
+//  Copyright © 2016 Connor Goddard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
