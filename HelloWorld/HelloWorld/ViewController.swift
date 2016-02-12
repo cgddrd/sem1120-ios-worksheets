6abab02b1eef3a334fb94ffead58fff613233eb3
Connor Goddard <hello@connorlukegoddard.com>
2016-02-12 15:15:38 +0000

    Added UISegmentedController for switching between images, added textbox and label, setup correct UI constraints and added a delegate for the return of a textfield.

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
+    // CG - IBOutlets represent "links" to the UI components.
     @IBOutlet weak var worldImage: UIImageView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,12 +27,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-    
     @IBAction func buttonPressed(sender: AnyObject) {
         
-        worldImage.image = UIImage(named: "World_Image_2")
+        if let text = textField.text where !text.isEmpty {
+            
+            messageLabel.text = "Hello, \(textField.text!)";
+            
+        } else {
+            // CG - Create the alert.
+            let alert = UIAlertController(title: "Error", message: "Textbox is empty.", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            // CG - Add an action (button).
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            
+            // CG - Show the alert.
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
         
+    }
+
+    @IBAction func segControlPressed(sender: AnyObject) {
+        
+        let segmentControl = sender as? UISegmentedControl;
+        
+        let segmentIndex = segmentControl?.selectedSegmentIndex;
+        
+        if (segmentIndex == 0) {
+            
+            worldImage.image = UIImage(named: "World_Image_1")
+            
+        } else {
+            
+            worldImage.image = UIImage(named: "World_Image_2")
+
+        }
+    }
+    
+    // CG - This function is part of the 'UITextFieldDelegate' protocol, which we have set as a delegate on the textfield component in order for it to be linked with the protocol, and allow this function to "listen" for events from the textfield component on the UI.
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        // CG - Trigger a 'press' event on the button.
+        buttonPressed(self)
+        
+        // CG - Remove the keyboard from the screen.
+        textField.resignFirstResponder()
+        
+        // Return true as we want to allow the textfield to return.
+        return true
     }
 
 }
